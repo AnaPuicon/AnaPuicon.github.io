59b85e379523d030c5be441ea05040a6c1b705f3
Banana <anitapui@gmail.com>
2024-10-18 21:38:55 -0500

    Actualizacion responsiva del navbar

--- hamburger.js
+++ hamburger.js
@@ -1,17 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var hamburger = document.querySelector('.hamburger');
-    var navList = document.getElementById('nav-list');
-    
-    // Función para alternar la visibilidad del menú
-    function toggleMenu() {
-        navList.classList.toggle('active');
-    }
-    
-    // Evento de clic para el botón de hamburguesa
-    hamburger.addEventListener('click', toggleMenu);
-    
-    // Eventos de clic para cada enlace en el menú de hamburguesa
-    document.querySelectorAll('#nav-list a').forEach(function(navItem) {
-        navItem.addEventListener('click', toggleMenu);
-    });
+document.getElementById('toggle-menu').addEventListener('click', function() {
+    const navItems = document.querySelector('.nav-items');
+    navItems.classList.toggle('active');
 });
